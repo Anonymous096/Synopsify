28ba20117b25fedfa1645b0a24bb77d10eb398ea
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-08-22 14:20:38 +0530

    Theme Colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        cream: "#f7efe5",
+        lightPink: "#e2bfd9",
+        softPurple: "#c8a1e0",
+        darkPurple: "#674188",
+      },
       fontFamily: {
         satoshi: ["Satoshi", "sans-serif"],
         inter: ["Inter", "sans-serif"],
